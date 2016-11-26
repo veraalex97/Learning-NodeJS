510e0d84cd7f6f1b8e8107ad1695e4eb6a33eecc
Alex Vera <veraalex97@gmail.com>
2016-11-25 21:52:46 -0500

    added an example of a non-blocking js server. this an example of how node is always listening to events (eventlooping concept). what this does is: it waits for a request then runs. Now, what this script does is, it waits for a request gives the 200 code waits, then continues with the response then ends.)

--- firstSteps/helloDog.js
+++ firstSteps/helloDog.js
@@ -15,9 +15,21 @@
 
 var http = require('http');
 
+// http.createServer(function(request, response) {
+//   response.writeHead(200);
+//   response.write("Hello World");
+//   response.end();
+// }).listen(8080);
+// console.log("Listening on http://localhost:8080");
+
+//This is an example of a non blocking server (manner of writing code)
+
 http.createServer(function(request, response) {
   response.writeHead(200);
-  response.write("Hello World");
-  response.end();
-}).listen(8080);
-console.log("Listening on http://localhost:8080");
+  response.write("Dog is running...")
+  setTimeout(function() { //after the server recieves the request, it waits then continues to do work
+    response.write("Dog is finished running");
+    response.end(); //ends connection (closes it)
+  }, 5000); //This is the amount of seconds (in millisecons of the wait time before the request keeps running)
+}).listen(8080); //listens on 8080
+console.log("Listening on http://localhost:8080")
